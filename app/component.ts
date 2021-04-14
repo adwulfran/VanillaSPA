fff5d4787d20df76350b81fd9cc51bf631bd660a
adwulfran <adrienwulfran@gmail.com>
2021-04-14 15:07:12 +0200

    remove uselesss line

--- app/component.ts
+++ app/component.ts
@@ -3,13 +3,13 @@ declare module globalThis {
 }
 
 export class Component {
-    
+
     constructor() {
-        
+
     }
-    
-    render(el:any) {
-        if (globalThis.routes.findIndex((x:any) => x.path === el['path']) !== -1) {
+
+    render(el: any) {
+        if (globalThis.routes.findIndex((x: any) => x.path === el['path']) !== -1) {
             var root = document.getElementById('root');
             root.innerHTML = `<` + el['path'] + `>` + el['template'] + `</` + el['path'] + `>`;
             var style = document.createElement('style');
@@ -23,13 +23,8 @@ export class Component {
             nested_comp.innerHTML = el['template'];
             nested_comp.appendChild(style);
             var script = document.createElement('script');
-            if (el['path'] === 'home-component') {
-                script.src = "../dist/" + el['path'] + ".ts"
-            }
-            else {
-                script.src = "../dist/" + el['path'] + ".js"
-            }
-            nested_comp.appendChild(script)
+            script.src = "../dist/" + el['path'] + ".js"
+            nested_comp.appendChild(script);
         }
     }
 
